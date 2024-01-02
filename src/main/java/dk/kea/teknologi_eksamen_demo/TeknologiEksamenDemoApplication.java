39e68fac7d3e5828ba97898ebe2b9e7425de8710
Teller501 <ande777j@stud.kea.dk>
2024-01-02 10:42:10 +0100

    initial commit

---
+++ src/main/java/dk/kea/teknologi_eksamen_demo/TeknologiEksamenDemoApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.teknologi_eksamen_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknologiEksamenDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeknologiEksamenDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/teknologi_eksamen_demo/TeknologiEksamenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.teknologi_eksamen_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknologiEksamenDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
